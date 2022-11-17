08405a5c7551e9e060a497cfc08520abeafaa5ba
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-11-17 12:59:57 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Downloading Update File"
 sleep 2

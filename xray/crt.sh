9da98cc836d36f693b5e26c48e59deabfd1db802
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-11-17 13:00:54 +0700

    Update crt.sh

--- xray/crt.sh
+++ xray/crt.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 colornow=$(cat /etc/ssnvpn/theme/color.conf)
 NC="\e[0m"

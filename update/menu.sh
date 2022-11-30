3e8133d6dc79b82394b8597545e40905c981868a
jhoker <jhokerhacker@gmail.com>
2022-12-01 04:13:03 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -145,7 +145,7 @@ echo -e "$COLOR1┌────────────────────�
 echo -e "$COLOR1│$NC Version     :${COLOR1} $(cat /opt/.ver) Latest Version${NC}"
 echo -e "$COLOR1│$NC User Roles  : $uis"
 echo -e "$COLOR1│$NC Client Name : $Name"
-if [ $exp = 1000 ];
+if [ $Exp == 1000 ];
 then
     echo -e "$COLOR1│$NC License     : Lifetime"
 else
